3c63f00eae888f39541663cb283ce795c9dc1b83
Barel Shraga <barel123133@gmail.com>
2022-05-21 13:17:46 +0300

    Updates

---
+++ precache-manifest.cc10d392a11d41e5fd59e42ce14f140e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6cd1693f9dc6cd3f0e3728e62297b6",
+    "url": "/travel_advisor/index.html"
+  },
+  {
+    "revision": "b0d87bf8b404030ec4f6",
+    "url": "/travel_advisor/static/js/2.3d325924.chunk.js"
+  },
+  {
+    "revision": "982d859dc350987e59dc50a6ca07812f",
+    "url": "/travel_advisor/static/js/2.3d325924.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "678e8c6a64815ec0a60f",
+    "url": "/travel_advisor/static/js/main.f207e370.chunk.js"
+  },
+  {
+    "revision": "4602604ca5c4b8d40873",
+    "url": "/travel_advisor/static/js/runtime-main.297c729e.js"
+  }
+]);
